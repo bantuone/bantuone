eff158c22d64e2bcde81c209424c78c01b2b3bdf
Febiana Nur Ramdani <fenaramdani@gmail.com>
2022-12-28 14:26:15 +0700

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
           title: 'bantuone',
           theme: themeData,
           onGenerateRoute: CustomRouter.generateRoute,
-          initialRoute: home,
+          initialRoute: login,
         ),
         ),
     );
