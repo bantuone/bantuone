51174d9016a36610f3e7fcf49e53a4a7b576200e
nadhirrahman <alifrahman164@gmail.com>
2022-12-29 20:04:02 +0700

    fix error

--- lib/core/provider_setup.dart
+++ lib/core/provider_setup.dart
@@ -1,10 +1,10 @@
 
-//import 'package:bantuone/features/order/viewmodel/order_viewmodel.dart';
+import 'package:bantuone/features/order/viewmodel/order_viewmodel.dart';
 
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 List<SingleChildWidget> providers = [
 
- // ChangeNotifierProvider(create: (_) => OrderViewModel()),
+  ChangeNotifierProvider(create: (_) => OrderViewModel()),
 ];
